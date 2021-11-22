7395b1e0197d6f56169a3966fdcf860b50a246c6
ME9799 <93213577+ME9799@users.noreply.github.com>
2021-11-22 22:48:33 +0300

    Add files via upload

---
+++ do assignment .2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int i=12;
+    do
+    {
+        cout<<"Enter a number: ";
+        
+        
+ 
+    }while( i <= 28){
+      if(i % 2 == 0){
+   
+         cout <<i<< " ";
+      }
+  
+     i++;
+ }
+ 
+ return 0;
+}
